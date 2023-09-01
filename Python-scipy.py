65a66f6ccbe5f9d8e3358841493d63d69a411c93
Jaycob <67782936+Jaycob-jh@users.noreply.github.com>
2023-09-01 13:11:50 +0800

    Update Python-scipy.py

--- Python-scipy.py
+++ Python-scipy.py
@@ -1,1 +1,15 @@
 import numpy as np
+from scipy import signal
+
+def apply_filter(data, lowcut, highcut, fs):
+  nyquist = 0.5 * fs
+  low = lowcut / nyquist
+  high = highcut / nyquist
+  b, a = signal.butter(5, [low, high], btype = 'band')
+  filter_data = signal.filtfilt(n, a, data)
+  return filter_data
+
+divided_data = {}
+#put all bands (1-160Hz) into the All band
+all_filtered_data = apply_filter(eeg_data, 1, 160, fs)
+divided_data['All'] = all_filtered_data
