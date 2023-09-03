9d03229a15d065b7abe2a01e6c2d20fb3f7f565b
Jaycob <67782936+Jaycob-jh@users.noreply.github.com>
2023-09-03 19:38:50 +0800

    Update create_epoch.py

--- create_new_epoch.py
+++ create_new_epoch.py
@@ -8,20 +8,31 @@ import mne
 
 path = '/Users/cuilab/Desktop/matlabworkspace/Brainproject/original_raw_data/'
 rawdata = mne.io.read_raw_eeglab(path + 'sub03.set', preload=True)
-# print(rawdata.info['ch_names'])
+rawdata.drop_channels(['CB1', 'CB2', 'TRIGGER'])
 
-#create the new eeg epoch for adding the channel informations
+montage = mne.channels.make_standard_montage("standard_1005")
+"""
+The neuroscan montage is not standard! We should change them!
+"""
+UM_rename_dict = {"FP1": "Fp1",
+                  "FP2": "Fp2",
+                  "FPZ": "Fpz",
+                  "FZ": "Fz",
+                  "FCZ": "FCz",
+                  "CZ": "Cz",
+                  "CPZ": "CPz",
+                  "OZ": "Oz",
+                  "PZ": "Pz",
+                  "POZ": "POz"}
+rawdata.rename_channels(UM_rename_dict)
+rawdata.set_montage("standard_1005")
 
-data = rawdata.get_data()
-exclude_rows = [60, 62, 63]
-data = [row for index, row in enumerate(data) if index + 1 not in exclude_rows]
-
-exclude_channel = ['CB1', 'CB2', 'TRIGGER']
-channel_dict = {value: index + 1 for index, value in enumerate(rawdata.info['ch_names']) if index + 1 not in exclude_channel}
-channel_type = ['eeg'] * 63
+channel_type = ['eeg'] * 60
+channel_dict = {value: index + 1 for index, value in enumerate(rawdata.info['ch_names'])}
 channel_names = list(channel_dict.keys())
 channel_index = list(channel_dict.values())
 n_channel = len(channel_index)
 info = mne.create_info(sfreq=1000, ch_names=channel_names, ch_types=channel_type)
 info.set_montage("standard_1005")
+data = rawdata.get_data()
 raw = mne.io.RawArray(data, info)
